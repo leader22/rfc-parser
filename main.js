65a90dfc2ccfaaf8d11d1e0024e3f6714dd25054
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-01-23 13:41:15 +0900

    Fix example

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ const { parse } = require('.');
 const no = '5389';
 // const no = '8445';
 const html = fs.readFileSync(`./txt/rfc${no}.txt`, { encoding: 'utf8' });
-parse(html);
+const { meta, sections } = parse(html);
+
+console.log(`# ${meta.title}`);
+for (const section of sections) {
+  if (!section.title.startsWith('2.')) { continue; }
+
+  console.log(`## ${section.title}`);
+  const body = section.body.map(b => b.value).join('\n');
+  console.log(body);
+}
