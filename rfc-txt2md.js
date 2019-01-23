a373e14573f16c3235c91c3e16127517d51efd2e
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-01-23 19:10:30 +0900

    Clean up repos

--- rfc-txt2md.js
+++ rfc-txt2md.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const { parse } = require('.');
 
-// const no = '5389';
-const no = '8445';
-const html = fs.readFileSync(`./txt/rfc${no}.txt`, { encoding: 'utf8' });
+const html = fs.readFileSync('./txt/rfc5389.txt', { encoding: 'utf8' });
 const { meta, sections } = parse(html);
 
 console.log(`# ${meta.title}`);
